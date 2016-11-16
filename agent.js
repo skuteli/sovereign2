3c753828ee5e147ab0e7da3dc3e1295fc95e9aca
skuteli <skuteli@outlook.com>
2016-11-16 12:01:24 +0100

    Training

--- agent.js
+++ agent.js
@@ -1,67 +1,102 @@
 class Agent {
 
-constructor() {
-    this.hp = 10;
-    this.lifeID = setInterval(()=>{this.live()},
-        100
-        );
-    return this;
-}
-
-die() {
-    console.log("I'm dying! Aaaargh!")
-    clearInterval(this.lifeID)
-    let index = agents.indexOf(this);
-    if (index > -1) {
-        agents.splice(index, 1);
-    } 
-}
-
-breed() {
-    this.hp = this.hp-10
-    if (Math.random() > 0.5)
-    {agents.push(new Gatherer)}
-    else
-    {agents.push(new Hunter)}
-}
-
-updateState() {
-    if (this.hp < 0) {
-        this.die();
-        return; 
+    constructor() {
+        this.hp = 15;
+        this.trainingTreshold = 0;
+        this.trainingPoints =0;
+        this.state = StateMachine.create({
+          initial: "training",
+          events: [
+          { name: 'finishTraining',  from: 'training',  to: 'idle' },
+          { name: 'goToWork', from: 'idle', to: 'working'    },
+          { name: 'goIdle',  from: 'working',    to: 'idle' }
+          ]});
+        this.lifeID = setInterval(()=>{this.live()},
+            100
+            );
+        return this;
     }
-    else if (this.hp > 25) {
-        this.breed()
+
+    die() {
+        console.log("I'm dying! Aaaargh!")
+        clearInterval(this.lifeID)
+        let index = agents.indexOf(this);
+        if (index > -1) {
+            agents.splice(index, 1);
+        } 
     }
-}
 
-work() {
-    console.log("I can't do anything:(")
-}
+    breed() {
+        this.hp = this.hp-10
+        if (Math.random() > 0.5)
+            {agents.push(new Gatherer)}
+        else
+            {agents.push(new Hunter)}
+    }
 
-updateStats() {
-    this.work()
-    this.hp--
-}
+    updateState() {
+        if (this.hp < 0) {
+            this.die();
+            return; 
+        }
+        else if (this.state.current != "training" && this.hp > 17) { //fundakurwamentalne
+            this.breed()
+        }
 
-live() {
-    this.updateStats()
-    this.updateState()
-}
+        if (this.state.current == "working")   { this.work()      }
+        else if (this.state.current == "idle") { this.findWork()  }
+        else if (this.state.current == "training") { this.train() }
+    }
+
+    findWork() {
+        console.log("I can't find work cause i don't know how to work:(")
+    }
+    train() {
+        if (this.trainingPoints>this.trainingTreshold) this.state.finishTraining();
+        this.trainingPoints++
+    }
+    work() {
+        console.log("I can't do anything:(")
+    }
+
+    updateStats() {
+        this.hp--
+    }
+
+    live() {
+        this.updateStats()
+        this.updateState()
+    }
 }
 
 class Gatherer extends Agent {
+    constructor() {
+        super();
+        this.trainingTreshold = 10;
+    }
     work() {
-        this.hp = this.hp + 1.05;
+        this.hp = this.hp + 1.1;
+    }
+        findWork() {
+            // TODO: find asset
+            this.state.goToWork()
     }
 }
 
 class Hunter extends Agent {
+    constructor() {
+        super();
+        this.trainingTreshold = 20;
+    }
     work() {
         if (Math.random() > 0.95) {
-        console.log("I caught something! Yay!");
-        this.hp = this.hp + 21;
+            console.log("I caught something! Yay!");
+            this.hp = this.hp + 24;
+        }
     }
+            findWork() {
+            // TODO: find asset
+            this.state.goToWork()
     }
 }
 
@@ -71,7 +106,12 @@ class Chieftain extends Agent {
         console.log("I'm ALIVE!!!");
         super();
         this.tribe = agents;
-        this.hp = 5;
+        this.hp = 15;
+        this.trainingTreshold = 10;
+    }
+            findWork() {
+            // TODO: find asset
+            this.state.goToWork()
     }
     work() {
         let sorted = this.tribe.slice().sort((first,second)=>{return first.hp-second.hp});
